e248e9747fe1150bc4139e4d4a0c287be86e1912
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-01-23 22:23:23 -0800

    add api capabilities

--- client/app/app.js
+++ client/app/app.js
@@ -14,15 +14,64 @@ angular.module('foodapp', ['ngRoute'])
       .otherwise({
         redirectTo: '/welcome'
       });
+
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
 
   //build services here
+  //define a factory that will return an object with serv
+  .factory('yelper', function($http) {
+    //define variable to close to keep track of place
+    var useLocation;
+    var setLocation = function(location) {
+      useLocation = location.split(' ');
+      useLocation = useLocation.join('+');
+      console.log('LOCATION SET TO THE FOLLOWING', useLocation);
+      return;
+    };
+    var getYelpEntry = function() {
+      var headers = {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      };
+      var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=food+in+' + useLocation + '&key='+ GOOGLE_API_KEY
+      return $http({
+        method: "GET",
+        headers: headers,
+        url: url
+      })
+      .then(function successCallback(response) {
+        var result = response.data.results
+        var randomNum = Math.floor(Math.random() * result.length);
+        return response.data.results[randomNum];
+      }, function errorCallback(response) {
+        return response;
+      });
+    };
 
-
-  //build all controllers here
-  .controller('welcomeCtrl', function() {
-
+    return {
+      setLocation: setLocation,
+      getYelpEntry: getYelpEntry
+    }
   })
 
-  .controller('foodCtrl', function() {
+  //build all controllers here
+  .controller('welcomeCtrl', function($scope, yelper) {
+    $scope.clicked = function () {
+      yelper.setLocation($scope.place);
+    }
+  })
+
+  .controller('foodCtrl', function($scope, yelper, $http) {
+    $scope.data = yelper.getYelpEntry().then(function(response) {
+      $scope.data = response;
+      return;
+    }, function errorCallback(response) {
+      return response;
+    });
   });
+
--- server.js
+++ server.js
@@ -7,6 +7,13 @@ var app = express();
 
 app.use(express.static(path.join(__dirname + '/client')));
 
+app.all("/api/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    return next();
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
